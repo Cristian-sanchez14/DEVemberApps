e56af23baaf89c33e43b1d8bff3309fd7b26c14d
Cristian Sanchez <cristiansanchez@Cristians-MacBook-Pro.local>
2023-12-07 10:32:38 -0700

    headerShown added to home page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, FlatList, View } from 'react-native';
 import DayListItem from '@components/core/DayListItem';
+import { Stack } from 'expo-router';
 
 const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{ headerShown: false }} />
       <FlatList
         data={days}
         contentContainerStyle={styles.content}
